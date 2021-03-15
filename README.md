70b4409f032af0e3aa0f24ba19a784a9f0e84e84
Rohan Deore <deorerohan@gmail.com>
2021-03-15 23:47:01 +0530

    Delete page and button added

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ASP_Dot_Net_Core
 Sample related to ASP .Net core
+
+## Create razor page 
+
+### Install code generator
+
+**dotnet tool install -g dotnet-aspnet-codegenerator**
+
+Refer [ASP-codegenerator](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/tools/dotnet-aspnet-codegenerator?view=aspnetcore-5.0) for more information.
+
+### Create a page
+
+**dotnet aspnet-codegenerator razorpage Delete Empty -outDir Pages/Restaurants/**
+
+dotnet : application
+aspnet-codegenerator : scaffolding engine
+razorpage : template
+Delete : page name
+Empty : UI template
+
+
+
+
+## Need to write some information
+
+- DbContext
+- Dotnet EF
+- database miration, update etc.
+- 
---
+++ WebUI/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Data;
+using Web.Core;
+
+namespace WebUI.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant{ get; set; }
+
+        public DeleteModel(IRestaurantData data)
+        {
+            restaurantData = data;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = restaurantData.Delete(restaurantId);
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
