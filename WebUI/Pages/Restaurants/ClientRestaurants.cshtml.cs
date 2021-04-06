b50afebf4b1e1b4ba223024e6349c2550b39ee8a
Rohan Deore <deorerohan@gmail.com>
2021-04-06 12:32:34 +0530

    Added API but not scuccessfull

---
+++ WebUI/Pages/Restaurants/ClientRestaurants.cshtml.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Data;
+using Web.Core;
+
+namespace WebUI.Pages.Restaurants
+{
+    public class ClientRestaurants : PageModel
+    {
+
+    }
+}
